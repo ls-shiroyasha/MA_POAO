df18ca3c22d24397878ce4261e186d0da2469e25
Luisiisho <juanluismoor@outlook.com>
2023-07-14 17:11:23 -0600

    Examen 2P

--- Examen2P/app.py
+++ Examen2P/app.py
@@ -11,8 +11,6 @@ app.config['MYSQL_DB']='DB_Floreria'#BD de la fruteria
 app.secret_key = "clave_secreta"#Token para validación por cuestiones de seguridad al mandar datos entre la BD y la página web
 mysql = MySQL(app)
 
-
-
 #Declaración de las ruta http://localhost:5000
 #Hacer que el servidor trabaje en el puerto especifícado: 5000 para este caso
 #Ejecución del servidor en el puerto 5000
@@ -24,91 +22,76 @@ def index():
     print(consultaDatos)
     return render_template('index.html')
 
-"""
+
 #Ruta http:localhost:5000/guardar - tipo POST para Insert
 @app.route('/guardar', methods = ['POST'])
 def guardar():
     if request.method == 'POST':
         #Pasamos a variables el contenido de los inputs
-        Vfruta = request.form['txtFruta']
-        Vtemporada = request.form['txtTemporada']        
-        Vprecio = request.form['txtPrecio']
-        Vstock = request.form['txtStock']
+        VNombre = request.form['txtNombre']
+        VCantidad = request.form['txtCantidad']        
+        Vprecio = request.form['txtPrecio']        
         #Conectar y ejecutar el insert
         #El objeto tipo cursor llamado CS
         CS = mysql.connection.cursor()   #(%s, %s, %s) --> Datos indefinidos que se pasan al siguiente parámetro de execute
-        CS.execute('insert into tbFrutas(Fruta, Temporada, Precio, Stock) values(%s, %s, %s, %s)', (Vfruta, Vtemporada, Vprecio, Vstock))
+        CS.execute('insert into tbFlores(Nombre, Cantidad, Precio) values(%s, %s, %s)', (VNombre, VCantidad, Vprecio))
         mysql.connection.commit()            
     flash('Los datos se agregaron correctamente')    
-    return redirect(url_for('registrar'))#Redireccionamos al index
-    #return render_template('registros.html')
-    #return redirect(url_for('index'))#Redireccionamos al index
+    return redirect(url_for('index'))#Redireccionamos al index
 
 @app.route('/editar/<id>')
 def editar(id):
     cursoId = mysql.connection.cursor()
-    cursoId.execute('select * from tbFrutas where id=%s', (id,))
+    cursoId.execute('select * from tbFlores where id=%s', (id,))
     consultId = cursoId.fetchone()
-    return render_template('editarRegistro.html', frut = consultId)
+    return render_template('editar.html', flor = consultId)
 
 @app.route('/actualizar/<id>', methods = ['POST'])
 def actualizar(id):
     if request.method == 'POST':
         #Pasamos a variables el contenido de los inputs
-        Vfruta = request.form['txtFruta']
-        Vtemporada = request.form['txtTemporada']        
-        Vprecio = request.form['txtPrecio']
-        Vstock = request.form['txtStock']
+        Vnombre = request.form['txtNombre']
+        Vcantidad = request.form['txtCantidad']        
+        Vprecio = request.form['txtPrecio']        
         #Conectar y ejecutar el insert
         #El objeto tipo cursor llamado CS
         curAct = mysql.connection.cursor()
-        curAct.execute('update tbFrutas set Fruta = %s, Temporada = %s, Precio = %s, Stock = %s where id = %s',(Vfruta, Vtemporada, Vprecio, Vstock, id))
+        curAct.execute('update tbFlores set Nombre = %s, Cantidad = %s, Precio = %s where id = %s',(Vnombre, Vcantidad, Vprecio, id))
         mysql.connection.commit()
     flash('Se actualizo el registro')
-    return redirect(url_for('registrar'))
-
-@app.route('/registrar')#Ruta raíz/principal o index
-def registrar():
-    CC = mysql.connection.cursor();#Cursor para hacer conexión a mysql
-    CC.execute('select * from tbFrutas')#Seleccionamos todos los datos de la tabla
-    consultaDatos = CC.fetchall()#Consultar datos de toda la tabla    
-    return render_template('registros.html', listFrutas = consultaDatos)
+    return redirect(url_for('index'))
 
 @app.route('/eliminar/<id>')####
 def eliminar(id):####
     cursoId = mysql.connection.cursor()
-    cursoId.execute('select * from tbFrutas where id=%s', (id,))
+    cursoId.execute('select * from tbFlores where id=%s', (id,))
     consultId = cursoId.fetchone()
-    return render_template('eliminarRegistro.html', frut = consultId)
-    
+    return render_template('eliminarFlor.html', flor = consultId)
+
 @app.route('/borrar/<id>', methods = ['POST'])
 def borrar(id):
     if request.method == 'POST':
         curAct = mysql.connection.cursor()
-        curAct.execute('delete from tbFrutas where id = %s', (id,))
+        curAct.execute('delete from tbFlores where id = %s', (id,))
         mysql.connection.commit()
-    flash('Se elimino el registro')
-    return redirect(url_for('registrar'))
+    flash('Se elimino la flor')
+    return redirect(url_for('index'))
 
 @app.route('/buscar')
 def buscar():
-    #CC = mysql.connection.cursor();#Cursor para hacer conexión a mysql
-    #CC.execute('select * from tbFrutas')#Seleccionamos todos los datos de la tabla
-    #consultaDatos = CC.fetchall()#Consultar datos de toda la tabla    
-    return render_template('hacerBusqueda.html')
+    return render_template('Busqueda.html')
 
 @app.route('/mostrar', methods = ['POST'])
 def mostrar():
     if request.method == 'POST':        
-        Vfruta = request.form['txtFruta']
+        Vflor = request.form['txtFlor']
         CC = mysql.connection.cursor()
-        CC.execute('select * from tbFrutas where Fruta=%s', (Vfruta,))
-        #CC.execute('select * tbFrutas where Fruta like = %s%', (Vfruta,))
+        CC.execute('select * from tbFlores where Nombre=%s', (Vflor,))        
         consultaDatos = CC.fetchall()        
-    flash('Coincidencias para: ' + Vfruta)
-    return render_template('hacerBusqueda.html', listFrutas = consultaDatos)
+    flash('Coincidencias para: ' + Vflor)
+    return render_template('Busqueda.html', listFlores = consultaDatos)
 
-"""
+
 #Habilitamos que la app cargue en servidor en el puerto especificado
 #El debug nos permite hacer cambios en el código sin tener que tumbar el servidor
 if __name__ == '__main__':
